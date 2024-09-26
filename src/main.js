04d6055f6b0aa2e13519e409d34fb2c57f1e6535
enlian <frejaenlian@gmail.com>
2024-09-27 02:36:49 +0900

    修改bug

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import commonJs from './assets/common.js';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import commonJs from './assets/common.js'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI);
-const moment = require('moment');
-require('moment/locale/zh-cn');
+Vue.use(ElementUI)
+const moment = require('moment')
+require('moment/locale/zh-cn')
 
 Vue.use(require('vue-moment'), {
   moment
-});
+})
 router.beforeEach((to, from, next) => {
-  typeof to.meta.pageTitle !== undefined && commonJs.setTitle(to.meta.pageTitle);
+  typeof to.meta.pageTitle !== undefined && commonJs.setTitle(to.meta.pageTitle)
   next()
 })
 
